42693453828b681514f2544c94e2a9bf0168616d
Timeuh <timeuuh@gmail.com>
2023-06-28 22:20:23 +0200

    feat: create function to change theme

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import {createContext, useState} from 'react';
 import {lang, theme} from '@/utils/AppTypes';
 import Home from '@/components/Home';
 import Navbar from '@/components/Navbar';
+import {isLight} from '@/utils/AppFuncs';
 
 // context for app theme
 export const ThemeContext = createContext<theme>('light');
@@ -14,11 +15,16 @@ export default function App() {
   // create and init lang context
   const [lang, setLang] = useState<lang>('fr');
 
+  // function to change theme in components
+  const changeTheme = () => {
+    isLight(theme) ? setTheme('dark') : setTheme('light');
+  };
+
   return (
     <div id='App'>
       <ThemeContext.Provider value={theme}>
         <LangContext.Provider value={lang}>
-          <Navbar />
+          <Navbar changeTheme={changeTheme}/>
           <Home />
         </LangContext.Provider>
       </ThemeContext.Provider>
