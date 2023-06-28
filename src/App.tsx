af040dbcc77bba505f151c8a818cf7d4f9ef8396
Timeuh <timeuuh@gmail.com>
2023-06-28 21:24:47 +0200

    refactor: extract context from function to use it everywhere

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,16 @@ import {createContext, useState} from 'react';
 import {lang, theme} from '@/utils/AppTypes';
 import Home from '@/components/Home';
 
+// context for app theme
+export const ThemeContext = createContext<theme>('light');
+// context for app langage
+export const LangContext = createContext<lang>('fr');
+
 export default function App() {
   // create and init theme context
   const [theme, setTheme] = useState<theme>('light');
-  const ThemeContext = createContext<theme>(theme);
-
   // create and init lang context
   const [lang, setLang] = useState<lang>('fr');
-  const LangContext = createContext<lang>(lang);
 
   return (
     <div id='App'>
