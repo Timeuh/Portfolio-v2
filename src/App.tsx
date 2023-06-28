627b69371fea78bbda4cffd505ae4f7e0ec3682d
Timeuh <timeuuh@gmail.com>
2023-06-28 20:33:50 +0200

    feat: provide lang to the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
 import {createContext, useState} from 'react';
-import {theme} from '@/utils/AppTypes';
+import {lang, theme} from '@/utils/AppTypes';
 
 export default function App() {
+  // create and init theme context
   const [theme, setTheme] = useState<theme>('light');
   const ThemeContext = createContext<theme>(theme);
+
+  // create and init lang context
+  const [lang, setLang] = useState<lang>('fr');
+  const LangContext = createContext<lang>(lang);
 
   return (
     <div id='App'>
       <ThemeContext.Provider value={theme}>
+        <LangContext.Provider value={lang}>
+
+        </LangContext.Provider>
       </ThemeContext.Provider>
     </div>
   );
