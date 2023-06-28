46c2fec7528a0e1e77161c05b0720cd658dd4cc7
Timeuh <timeuuh@gmail.com>
2023-06-28 20:28:19 +0200

    feat: provide theme to the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import {createContext, useState} from 'react';
+import {theme} from '@/utils/AppTypes';
+
 export default function App() {
+  const [theme, setTheme] = useState<theme>('light');
+  const ThemeContext = createContext<theme>(theme);
+
   return (
     <div id='App'>
-
+      <ThemeContext.Provider value={theme}>
+      </ThemeContext.Provider>
     </div>
   );
 }
