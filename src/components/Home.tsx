45dcbae1c42b474c29f19dd8c8f045bfe2fccb2a
Timeuh <timeuuh@gmail.com>
2023-06-28 21:26:23 +0200

    style: add dark theme

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,11 +1,23 @@
+import {useContext} from 'react';
+import {theme} from '@/utils/AppTypes';
+import {ThemeContext} from '@/App';
+
 export default function Home(){
+  // get the theme from provider
+  const theme = useContext<theme>(ThemeContext);
+
+  // check if the theme is light
+  const isLight = () => {
+    return theme === 'light';
+  };
+
   return (
-    <div className={'h-screen w-full bg-home-light-mobile bg-no-repeat bg-cover flex flex-col justify-center items-center space-y-8'}>
-      <h1 className={'text-6xl text-violet-dark font-bold text-center'}>Timothée Brindejonc</h1>
+    <div id={'home'} className={`home-base-mobile ${isLight() ? 'home-light-mobile' : 'home-dark-mobile'}`}>
+      <h1 className={`text-6xl font-bold text-center ${isLight() ? 'text-violet-dark' : 'text-pink-light'}`}>Timothée Brindejonc</h1>
       <img src='src/assets/images/sections/developer.png' alt='developer' className={'w-40 h-40'}/>
-      <h2 className={'text-5xl text-violet-dark w-2/3'}>Étudiant en
-        <span className={'text-pink-light font-bold'}> BUT Informatique</span> et futur développeur
-        <span className={'text-pink-light font-bold'}> Frontend</span>
+      <h2 className={`text-5xl w-2/3 ${isLight() ? 'text-violet-dark' : 'text-pink-light'}`}>Étudiant en
+        <span className={`font-bold ${isLight() ? 'text-pink-light' : 'text-primary-dark'}`}> BUT Informatique</span> et futur développeur
+        <span className={`font-bold ${isLight() ? 'text-pink-light' : 'text-primary-dark'}`}> Frontend</span>
       </h2>
     </div>
   );
