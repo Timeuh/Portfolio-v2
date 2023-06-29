ed6f48aa7c99f331128ba1e0d102968aca84b4bc
Timeuh <timeuuh@gmail.com>
2023-06-29 18:16:10 +0200

    fix: get back right path for images

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -12,7 +12,7 @@ export default function Home(){
   return (
     <div id={'home'} className={`home-base-mobile ${isCurrentLight ? 'home-light-mobile' : 'home-dark-mobile'}`}>
       <h1 className={`text-6xl font-bold text-center ${isCurrentLight ? 'text-violet-dark' : 'text-pink-light'}`}>Timothée Brindejonc</h1>
-      <img src='../../assets/images/sections/developer.png' alt='developer' className={'w-40 h-40'}/>
+      <img src='src/assets/images/sections/developer.png' alt='developer' className={'w-40 h-40'}/>
       <h2 className={`text-5xl w-2/3 ${isCurrentLight ? 'text-violet-dark' : 'text-pink-light'}`}>Étudiant en
         <span className={`font-bold ${isCurrentLight ? 'text-pink-light' : 'text-primary-dark'}`}> BUT Informatique</span> et futur développeur
         <span className={`font-bold ${isCurrentLight ? 'text-pink-light' : 'text-primary-dark'}`}> Frontend</span>
