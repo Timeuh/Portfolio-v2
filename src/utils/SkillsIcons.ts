5a7b5c1bd1f5e97c063e3422560203fbe71e0566
Timeuh <timeuuh@gmail.com>
2023-06-29 17:50:03 +0200

    feat: create skill icons arrays

--- src/utils/SkillsIcons.ts
+++ src/utils/SkillsIcons.ts
@@ -23,3 +23,109 @@ export const skillSectionsIcons: Array<skillIcon> = [
     name: 'learn'
   }
 ];
+
+export const skillIcons: Array<Array<skillIcon>> = [
+  [
+    {
+      dark: '../src/assets/images/skills/front-end/html-dark.png',
+      light: '../src/assets/images/skills/front-end/html-light.png',
+      name: 'HTML'
+    },
+    {
+      dark: '../src/assets/images/skills/front-end/css-dark.png',
+      light: '../src/assets/images/skills/front-end/css-light.png',
+      name: 'CSS'
+    },
+    {
+      dark: '../src/assets/images/skills/front-end/js-dark.png',
+      light: '../src/assets/images/skills/front-end/js-light.png',
+      name: 'Javascript'
+    },
+    {
+      dark: '../src/assets/images/skills/front-end/react-dark.png',
+      light: '../src/assets/images/skills/front-end/react-light.png',
+      name: 'React Js'
+    },
+    {
+      dark: '../src/assets/images/skills/front-end/tailwind-dark.png',
+      light: '../src/assets/images/skills/front-end/tailwind-light.png',
+      name: 'Tailwind CSS'
+    }
+  ],
+  [
+    {
+      dark: '../src/assets/images/skills/back-end/php-dark.png',
+      light: '../src/assets/images/skills/back-end/php-light.png',
+      name: 'PHP'
+    },
+    {
+      dark: '../src/assets/images/skills/back-end/oracle-dark.png',
+      light: '../src/assets/images/skills/back-end/oracle-light.png',
+      name: 'Oracle SQL'
+    },
+    {
+      dark: '../src/assets/images/skills/back-end/mysql-dark.png',
+      light: '../src/assets/images/skills/back-end/mysql-light.png',
+      name: 'MySQL'
+    },
+    {
+      dark: '../src/assets/images/skills/back-end/laravel-dark.png',
+      light: '../src/assets/images/skills/back-end/laravel-light.png',
+      name: 'Laravel'
+    }
+  ],
+  [
+    {
+      dark: '../src/assets/images/skills/tools/linux-dark.png',
+      light: '../src/assets/images/skills/tools/linux-light.png',
+      name: 'Linux'
+    },
+    {
+      dark: '../src/assets/images/skills/tools/bash-dark.png',
+      light: '../src/assets/images/skills/tools/bash-light.png',
+      name: 'Bash'
+    },
+    {
+      dark: '../src/assets/images/skills/tools/jetbrains-dark.png',
+      light: '../src/assets/images/skills/tools/jetbrains-light.png',
+      name: 'Jetbrains Tools'
+    },
+    {
+      dark: '../src/assets/images/skills/tools/figma-dark.png',
+      light: '../src/assets/images/skills/tools/figma-light.png',
+      name: 'Figma'
+    },
+    {
+      dark: '../src/assets/images/skills/tools/vite-dark.png',
+      light: '../src/assets/images/skills/tools/vite-light.png',
+      name: 'Vite Js'
+    },
+    {
+      dark: '../src/assets/images/skills/tools/firebase-dark.png',
+      light: '../src/assets/images/skills/tools/firebase-light.png',
+      name: 'Firebase'
+    }
+  ],
+  [
+    {
+      dark: '../src/assets/images/skills/learning/next-dark.png',
+      light: '../src/assets/images/skills/learning/next-light.png',
+      name: 'Next Js'
+    },
+    {
+      dark: '../src/assets/images/skills/learning/mongo-dark.png',
+      light: '../src/assets/images/skills/learning/mongo-light.png',
+      name: 'Mongo DB'
+    },
+    {
+      dark: '../src/assets/images/skills/learning/vue-dark.png',
+      light: '../src/assets/images/skills/learning/vue-light.png',
+      name: 'Vue Js'
+    },
+    {
+      dark: '../src/assets/images/skills/learning/nuxt-dark.png',
+      light: '../src/assets/images/skills/learning/nuxt-light.png',
+      name: 'Nuxt Js'
+    }
+  ]
+];
