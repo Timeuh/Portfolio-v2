ecc45b821c798c0e68e97e8703a5de603ce33da9
Timeuh <timeuuh@gmail.com>
2023-06-29 13:59:38 +0200

    feat: create experiences array

---
+++ src/utils/Experiences.ts
@@ -0,0 +1,17 @@
+import {experience} from '@/utils/AppTypes';
+
+// work experiences
+export const workExperiences: Array<experience> = [
+  {
+    company: 'GIBRALTAZ',
+    logo: '../src/assets/images/experience/gibraltaz.png',
+    period: 'Février-Avril 2023',
+    role: 'Développeur web (stage)'
+  },
+  {
+    company: 'PLUS À VENIR',
+    logo: '../src/assets/images/experience/soon-light.png',
+    period: 'Revenez plus tard',
+    role: 'Développe mes projets'
+  }
+];
