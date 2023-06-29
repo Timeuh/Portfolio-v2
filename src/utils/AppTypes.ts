c1278fd7d74d78b1f5286e17bb6a2f3a99a62eb3
Timeuh <timeuuh@gmail.com>
2023-06-29 13:58:39 +0200

    feat: create experience type

--- src/utils/AppTypes.ts
+++ src/utils/AppTypes.ts
@@ -3,3 +3,11 @@ export type theme = 'light' | 'dark';
 
 // langage for the app
 export type lang = 'fr' | 'en';
+
+// work experience
+export type experience = {
+  company: string,
+  logo: string,
+  period: string,
+  role: string
+}
