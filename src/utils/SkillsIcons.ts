3136b8881133586907d5de1f1e6a46a522f195cc
Timeuh <timeuuh@gmail.com>
2023-06-29 15:53:50 +0200

    feat: create type for skill section icon

---
+++ src/utils/SkillsIcons.ts
@@ -0,0 +1,25 @@
+import {skillIcon} from '@/utils/AppTypes';
+
+// array of all skill sections icons
+export const skillSectionsIcons: Array<skillIcon> = [
+  {
+    dark: '../src/assets/images/skills/sections/front-end-light.png',
+    light: '../src/assets/images/skills/sections/front-end-dark.png',
+    name: 'frontend'
+  },
+  {
+    dark: '../src/assets/images/skills/sections/back-end-light.png',
+    light: '../src/assets/images/skills/sections/back-end-dark.png',
+    name: 'backend'
+  },
+  {
+    dark: '../src/assets/images/skills/sections/tools-light.png',
+    light: '../src/assets/images/skills/sections/tools-dark.png',
+    name: 'tools'
+  },
+  {
+    dark: '../src/assets/images/skills/sections/learn-light.png',
+    light: '../src/assets/images/skills/sections/learn-dark.png',
+    name: 'learn'
+  }
+];
