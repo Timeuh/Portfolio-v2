f75f6f434a58bf1563395d21ee884ac68ecb8172
Timeuh <timeuuh@gmail.com>
2023-06-30 12:16:41 +0200

    feat: create about section traductions

--- src/utils/TextContent.ts
+++ src/utils/TextContent.ts
@@ -1,4 +1,4 @@
-import {homeTrad} from '@/utils/AppTypes';
+import {aboutTrad, homeTrad} from '@/utils/AppTypes';
 
 // home section traductions
 export const homeContent: homeTrad = {
@@ -15,5 +15,51 @@ export const homeContent: homeTrad = {
     firstSpan: ' Computer Science Bachelor',
     secondPart: ' and future',
     secondSpan: ' Frontend Developer'
+  }
+};
+
+// about section traductions
+export const aboutContent: aboutTrad = {
+  fr: {
+    title: 'Qui Suis-je ?',
+    firstPartOne: 'Je m’appelle Timothée Brindejonc et j’ai 21 ans. Je suis actuellement en 3ème année de ',
+    firstSpanOne: 'BUT Informatique ',
+    firstPartTwo: '(en alternance ?) en parcours ',
+    firstSpanTwo: 'Développement Web',
+    secondPartOne: ' J’ai pour projet de faire un bac +5, en Master ou bien en École d’ingénieur, et ce en ',
+    secondSpanOne: 'alternance',
+    secondPartTwo: ' Ensuite, je chercherai une entreprise qui travaille avec des technologies qui me plaisent / m’intéressent.',
+    thirdPartOne: 'En attendant, je fais des projets personnels sur des technologies qui me plaisent : principalement ',
+    thirdSpanOne: 'React',
+    thirdPartTwo: ' J’essaie aussi de découvrir des technologies que je ne connais pas, pour élargir mes horizons de connaissance.',
+    fourthPartOne: 'Sinon j’aime aussi lire des ',
+    fourthSpanOne: 'romans ',
+    fourthPartTwo: 'et des ',
+    fourthSpanTwo: 'mangas',
+    fourthPartThree: ', regarder des ',
+    fourthSpanThree: 'animés japonais',
+    fourthPartFour: ' avec mes amis et jouer aux ',
+    fourthSpanFour: 'jeux vidéos'
+  },
+  en: {
+    title: 'Who Am-i ?',
+    firstPartOne: 'I am Timothée Brindejonc and I\'m 21 years old. I am currently in third year of ',
+    firstSpanOne: 'Computer Science Bachelor ',
+    firstPartTwo: '(maybe in internship ?) with ',
+    firstSpanTwo: 'Web Development Option',
+    secondPartOne: ' I want to do 5 years of studies, in a Master Degree or in Engineering School, in ',
+    secondSpanOne: 'internship',
+    secondPartTwo: ' Then, I will look for a company which work with technologies I like / I interest with.',
+    thirdPartOne: 'Meanwhile, I\'m making personal projects with technologies I like : mainly ',
+    thirdSpanOne: 'React',
+    thirdPartTwo: ' I also try to discover technologies I don\'t already know, to broaden my horizons of knowledge.',
+    fourthPartOne: 'Otherwise, I also like to read ',
+    fourthSpanOne: 'novels ',
+    fourthPartTwo: 'and ',
+    fourthSpanTwo: 'mangas',
+    fourthPartThree: ', looking ',
+    fourthSpanThree: 'japanese animation',
+    fourthPartFour: ' with my friends and play ',
+    fourthSpanFour: 'video games'
   }
 };
