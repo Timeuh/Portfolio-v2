69846dc47a98a21e00b4d69d78877bbfbe5ddaff
Timeuh <timeuuh@gmail.com>
2023-06-30 12:17:10 +0200

    feat(refactor): use about section traductions

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -1,7 +1,8 @@
 import {useContext} from 'react';
-import {theme} from '@/utils/AppTypes';
-import {ThemeContext} from '@/App';
+import {lang, theme} from '@/utils/AppTypes';
+import {LangContext, ThemeContext} from '@/App';
 import {isLight} from '@/utils/AppFuncs';
+import {aboutContent} from '@/utils/TextContent';
 
 export default function About() {
   // get current theme
@@ -9,28 +10,36 @@ export default function About() {
   // check if current theme is light
   const isCurrentLight = isLight(theme);
 
+  // get the lang from provider
+  const lang = useContext<lang>(LangContext);
+  // get current traduction
+  const currentTrad = aboutContent[lang];
+
   return (
     <div id={'about'} className={`about-base-mobile ${isCurrentLight ? 'bg-pink-light text-violet-dark' : 'bg-violet-dark text-pink-light'}`}>
       <img src='src/assets/images/pictures/about-pic.jpg' alt='me' className={'w-20 h-20 rounded-full mt-14 object-cover object-top'}/>
-      <h1 className={'text-5xl pb-8 pt-2 font-bold'}>Qui suis-je ?</h1>
+      <h1 className={'text-5xl pb-8 pt-2 font-bold'}>{currentTrad.title}</h1>
       <div className={'flex flex-col items-center space-y-4 w-2/3'}>
-        <p>Je m’appelle Timothée Brindejonc et j’ai 21 ans. Je suis actuellement en 3ème année de
-          <span className={`font-bold ${isCurrentLight ? 'text-primary-light' : 'text-primary-dark'}`}> BUT Informatique </span>
-          (en alternance ?) en parcours
-          <span className={`font-bold ${isCurrentLight ? 'text-primary-light' : 'text-primary-dark'}`}> Développement Web</span>.
+        <p>{currentTrad.firstPartOne}
+          <span className={`font-bold ${isCurrentLight ? 'text-primary-light' : 'text-primary-dark'}`}>{currentTrad.firstSpanOne}</span>
+          {currentTrad.firstPartTwo}
+          <span className={`font-bold ${isCurrentLight ? 'text-primary-light' : 'text-primary-dark'}`}>{currentTrad.firstSpanTwo}</span>.
         </p>
-        <p>J’ai pour projet de faire un bac +5, en Master ou bien en École d’ingénieur, et ce en
-          <span className={`font-bold ${isCurrentLight ? 'text-primary-light' : 'text-primary-dark'}`}> alternance</span>.
-          Ensuite, je chercherai une entreprise qui travaille avec des technologies qui me plaisent / m’intéressent.
+        <p>{currentTrad.secondPartOne}
+          <span className={`font-bold ${isCurrentLight ? 'text-primary-light' : 'text-primary-dark'}`}>{currentTrad.secondSpanOne}</span>.
+          {currentTrad.secondPartTwo}
         </p>
-        <p>En attendant, je fais des projets personnels sur des technologies qui me plaisent : principalement
-          <span className={`font-bold ${isCurrentLight ? 'text-primary-light' : 'text-primary-dark'}`}> React</span>.
-          J’essaie aussi de découvrir des technologies que je ne connais pas, pour élargir mes horizons de connaissance.
+        <p>{currentTrad.thirdPartOne}
+          <span className={`font-bold ${isCurrentLight ? 'text-primary-light' : 'text-primary-dark'}`}>{currentTrad.thirdSpanOne}</span>.
+          {currentTrad.thirdPartTwo}
         </p>
-        <p>Sinon j’aime aussi lire des <span className={`font-bold ${isCurrentLight ? 'text-primary-light' : 'text-primary-dark'}`}>romans </span>
-          et des <span className={`font-bold ${isCurrentLight ? 'text-primary-light' : 'text-primary-dark'}`}> mangas</span>, regarder des
-          <span className={`font-bold ${isCurrentLight ? 'text-primary-light' : 'text-primary-dark'}`}> animés japonais </span>
-          avec mes amis et jouer aux <span className={`font-bold ${isCurrentLight ? 'text-primary-light' : 'text-primary-dark'}`}> jeux vidéos</span>.
+        <p>{currentTrad.fourthPartOne}<span className={`font-bold ${isCurrentLight ? 'text-primary-light' : 'text-primary-dark'}`}>
+          {currentTrad.fourthSpanOne}</span>{currentTrad.fourthPartTwo}
+        <span className={`font-bold ${isCurrentLight ? 'text-primary-light' : 'text-primary-dark'}`}>{currentTrad.fourthSpanTwo}</span>
+        {currentTrad.fourthPartThree}
+        <span className={`font-bold ${isCurrentLight ? 'text-primary-light' : 'text-primary-dark'}`}>{currentTrad.fourthSpanThree}</span>
+        {currentTrad.fourthPartFour}
+        <span className={`font-bold ${isCurrentLight ? 'text-primary-light' : 'text-primary-dark'}`}>{currentTrad.fourthSpanFour}</span>.
         </p>
       </div>
     </div>
