3d1c1e08856d0db42880f95222f84998dbbdeeff
Timeuh <timeuuh@gmail.com>
2023-06-30 11:40:49 +0200

    feat: create home content traductions

---
+++ src/utils/TextContent.ts
@@ -0,0 +1,19 @@
+import {homeTrad} from '@/utils/AppTypes';
+
+// home section traductions
+export const homeContent: homeTrad = {
+  fr: {
+    name: 'Timothée Brindejonc',
+    firstPart: 'Étudiant en',
+    firstSpan: ' BUT Informatique',
+    secondPart: ' et futur développeur',
+    secondSpan: ' Frontend'
+  },
+  en: {
+    name: 'Timothée Brindejonc',
+    firstPart: 'Student in',
+    firstSpan: ' Computer Science Bachelor',
+    secondPart: ' and future',
+    secondSpan: ' Frontend Developer'
+  }
+};
