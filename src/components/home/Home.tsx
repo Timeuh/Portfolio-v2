df7f6c611cee28342e8591662aa4f8f876698db7
Timeuh <timeuuh@gmail.com>
2023-06-30 11:41:11 +0200

    feat(refactor): use current lang for traduction

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,7 +1,8 @@
 import {useContext} from 'react';
-import {theme} from '@/utils/AppTypes';
-import {ThemeContext} from '@/App';
+import {lang, theme} from '@/utils/AppTypes';
+import {LangContext, ThemeContext} from '@/App';
 import {isLight} from '@/utils/AppFuncs';
+import {homeContent} from '@/utils/TextContent';
 
 export default function Home(){
   // get the theme from provider
@@ -9,13 +10,19 @@ export default function Home(){
   // check if the current theme is light
   const isCurrentLight = isLight(theme);
 
+  // get the lang from provider
+  const lang = useContext<lang>(LangContext);
+  // get current traduction
+  const currentTrad = homeContent[lang];
+
   return (
     <div id={'home'} className={`home-base-mobile ${isCurrentLight ? 'home-light-mobile' : 'home-dark-mobile'}`}>
-      <h1 className={`text-6xl font-bold text-center ${isCurrentLight ? 'text-violet-dark' : 'text-pink-light'}`}>Timothée Brindejonc</h1>
+      <h1 className={`text-6xl font-bold text-center ${isCurrentLight ? 'text-violet-dark' : 'text-pink-light'}`}>{currentTrad.name}</h1>
       <img src='src/assets/images/sections/developer.png' alt='developer' className={'w-40 h-40'}/>
-      <h2 className={`text-5xl w-2/3 ${isCurrentLight ? 'text-violet-dark' : 'text-pink-light'}`}>Étudiant en
-        <span className={`font-bold ${isCurrentLight ? 'text-pink-light' : 'text-primary-dark'}`}> BUT Informatique</span> et futur développeur
-        <span className={`font-bold ${isCurrentLight ? 'text-pink-light' : 'text-primary-dark'}`}> Frontend</span>
+      <h2 className={`text-5xl w-2/3 ${isCurrentLight ? 'text-violet-dark' : 'text-pink-light'}`}>{currentTrad.firstPart}
+        <span className={`font-bold ${isCurrentLight ? 'text-pink-light' : 'text-primary-dark'}`}>{currentTrad.firstSpan}</span>
+        {currentTrad.secondPart}<span className={`font-bold ${isCurrentLight ? 'text-pink-light' : 'text-primary-dark'}`}>
+          {currentTrad.secondSpan}</span>
       </h2>
     </div>
   );
