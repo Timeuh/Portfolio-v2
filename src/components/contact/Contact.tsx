f7d922825cc10cbc1a6ae40f9df70b7df5e7c0a6
Timeuh <timeuuh@gmail.com>
2023-07-02 15:47:32 +0200

    feat(refactor): use traductions for contact section

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -1,13 +1,19 @@
 import {useContext} from 'react';
-import {ThemeContext} from '@/App';
-import {theme} from '@/utils/AppTypes';
+import {LangContext, ThemeContext} from '@/App';
+import {lang, theme} from '@/utils/AppTypes';
 import {isLight} from '@/utils/AppFuncs';
+import {contactContent} from '@/utils/TextContent';
 
 export default function Contact() {
   // get current theme
   const theme = useContext<theme>(ThemeContext);
   // check if current theme is light
   const isCurrentLight = isLight(theme);
+
+  // get the lang from provider
+  const lang = useContext<lang>(LangContext);
+  // get current traduction
+  const currentTrad = contactContent[lang];
 
   return (
     <div id={'contact'} className={`h-screen w-full flex flex-col items-center justify-center space-y-8
@@ -17,7 +23,7 @@ export default function Contact() {
           <img src='src/assets/images/contact/contact-light.png' alt='contact'/>
           : <img src='src/assets/images/contact/contact-dark.png' alt='contact'/>
       }
-      <h1 className={`font-bold text-5xl ${isCurrentLight ? 'text-purple-dark' : 'text-violet-light'}`}>Contactez-moi</h1>
+      <h1 className={`font-bold text-5xl ${isCurrentLight ? 'text-purple-dark' : 'text-violet-light'}`}>{currentTrad.title}</h1>
       <div className={'w-5/6'}>
         <a href={'https://www.linkedin.com/in/timothee-brindejonc/'} className={`contact-link border-b-2 
           ${isCurrentLight ? 'border-green-dark' : 'border-green-light'}`}>
