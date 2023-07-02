a89226a2f16c68ef1e9e279d8793eb693c069d9b
Timeuh <timeuuh@gmail.com>
2023-07-02 15:41:56 +0200

    refactor: add traductions for projects

--- src/utils/Projects.ts
+++ src/utils/Projects.ts
@@ -63,8 +63,11 @@ export const projects: Array<project> = [
   {
     name: 'Pinit',
     gif: '../src/assets/images/projects/utils/pinit.gif',
-    description: 'Pinit est un outil en ligne de commandes qui permet de rapidement créer et commencer un projet web Javascript. ' +
-      'Il est également possible d’utiliser une template de projet pour commencer encore plus vite.',
+    description: {
+      fr: 'Pinit est un outil en ligne de commandes qui permet de rapidement créer et commencer un projet web Javascript. ' +
+        'Il est également possible d’utiliser une template de projet pour commencer encore plus vite.',
+      en: 'Pinit is a command line interface to rapidly bootstrap a Javascript web project. It is possible to use a project template to start faster.'
+    },
     techs: [
       allTechs.javascript,
       allTechs.typescript,
@@ -80,8 +83,12 @@ export const projects: Array<project> = [
   {
     name: 'Portfolio V1',
     gif: '../src/assets/images/projects/utils/portfolio-v1.gif',
-    description: 'La première version de mon portfolio était mon premier pas dans l’univers de React Js et l’utilisation  de Tailwind CSS.  ' +
-      'Il est hébergé avec Firebase, et toujours consultable pour  voir la progression entre la V1 et la  V2.',
+    description: {
+      fr: 'La première version de mon portfolio était mon premier pas dans l’univers de React Js et l’utilisation de Tailwind CSS. ' +
+        'Il est hébergé avec Firebase, et toujours consultable pour voir la progression entre la V1 et la  V2.',
+      en: 'The first version of my portfolio was my first steps in the universe of React Js and the use of Tailwind CSS. It is hosted with Firebase ' +
+        'and is still available to view to compare between the two versions of my portfolio.'
+    },
     techs: [
       allTechs.javascript,
       allTechs.react,
@@ -102,8 +109,12 @@ export const projects: Array<project> = [
   {
     name: 'Spotifree',
     gif: '../src/assets/images/projects/utils/spotifree.gif',
-    description: 'Spotifree est un site web de streaming de musique, programmé en PHP. Le style est fait avec Tailwind CSS. ' +
-      'Ce projet était mon premier site web, et je voulais essayer d\'utiliser Tailwind CSS.',
+    description: {
+      fr: 'Spotifree est un site web de streaming de musique, programmé en PHP. Le style est fait avec Tailwind CSS. ' +
+        'Ce projet était mon premier site web, et je voulais essayer d\'utiliser Tailwind CSS.',
+      en: 'Spotifree is a music streaming website programmed in PHP. The style is done with Tailwind CSS. This war my first web project, and' +
+        ' I wanted to try Tailwind CSS.'
+    },
     techs: [
       allTechs.php,
       allTechs.tailwind
@@ -118,8 +129,12 @@ export const projects: Array<project> = [
   {
     name: 'UHC Run',
     gif: '../src/assets/images/projects/utils/uhc-run.gif',
-    description: 'L\'UHC Run est mon premier projet de code, programmé en Java. Il s\'agit d\'un plugin minecraft permettant de jouer des parties' +
-      ' en multijoueur avec plusieurs ajouts au jeu de base.',
+    description: {
+      fr: 'L\'UHC Run est mon premier projet de code, programmé en Java. Il s\'agit d\'un plugin Minecraft Spigot permettant de jouer des parties' +
+        ' en multijoueur avec plusieurs ajouts au jeu de base.',
+      en: 'The UHC Run is mon first code project, programmed in Java. It is a Minecraft Spigot plugin which permits to play multiplayer games' +
+        ' with several additions to the main game.'
+    },
     techs: [
       allTechs.java
     ],
