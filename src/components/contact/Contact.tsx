d48f9eeb9f5b799ec10165f07032f20febfbdab2
Timeuh <timeuuh@gmail.com>
2023-07-03 21:08:46 +0200

    style: make desktop style for contact section

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -20,33 +20,33 @@ export default function Contact() {
       ${isCurrentLight ? 'bg-violet-light' : 'bg-purple-dark'}`}>
       {
         isCurrentLight ?
-          <img src='src/assets/images/contact/contact-light.png' alt='contact'/>
-          : <img src='src/assets/images/contact/contact-dark.png' alt='contact'/>
+          <img src='src/assets/images/contact/contact-light.png' alt='contact' className={'xl:w-40'}/>
+          : <img src='src/assets/images/contact/contact-dark.png' alt='contact' className={'xl:w-40'}/>
       }
-      <h1 className={`font-bold text-5xl ${isCurrentLight ? 'text-purple-dark' : 'text-violet-light'}`}>{currentTrad.title}</h1>
-      <div className={'w-5/6'}>
+      <h1 className={`font-bold text-5xl ${isCurrentLight ? 'text-purple-dark' : 'text-violet-light'} xl:text-7xl`}>{currentTrad.title}</h1>
+      <div className={'w-5/6 xl:w-1/5'}>
         <a href={'https://www.linkedin.com/in/timothee-brindejonc/'} className={`contact-link border-b-2 
           ${isCurrentLight ? 'border-green-dark' : 'border-green-light'}`}>
           {
             isCurrentLight ?
-              <img src='src/assets/images/contact/linkedin-light.png' alt='linkedin' className={'w-20 h-auto'}/>
-              : <img src='src/assets/images/contact/linkedin-dark.png' alt='linkedin' className={'w-20 h-auto'}/>
+              <img src='src/assets/images/contact/linkedin-light.png' alt='linkedin' className={'w-20 h-auto xl:w-32'}/>
+              : <img src='src/assets/images/contact/linkedin-dark.png' alt='linkedin' className={'w-20 h-auto xl:w-32'}/>
           }
           <h2 className={`text-4xl ${isCurrentLight ? 'text-green-dark' : 'text-green-light'}`}>LinkedIn</h2>
         </a>
         <a href={'https://github.com/Timeuh'} className={`contact-link border-b-2 ${isCurrentLight ? 'border-violet-dark' : 'border-violet-light'}`}>
           {
             isCurrentLight ?
-              <img src='src/assets/images/contact/github-light.png' alt='github' className={'w-20 h-auto'}/>
-              : <img src='src/assets/images/contact/github-dark.png' alt='github' className={'w-20 h-auto'}/>
+              <img src='src/assets/images/contact/github-light.png' alt='github' className={'w-20 h-auto xl:w-32'}/>
+              : <img src='src/assets/images/contact/github-dark.png' alt='github' className={'w-20 h-auto xl:w-32'}/>
           }
           <h2 className={`text-4xl ${isCurrentLight ? 'text-violet-dark' : 'text-pink-light'}`}>Github</h2>
         </a>
         <a href={'mailto:tbrindejonc@gmail.com'} className={'contact-link'}>
           {
             isCurrentLight ?
-              <img src='src/assets/images/contact/mail-light.png' alt='email' className={'w-20 h-auto'}/>
-              : <img src='src/assets/images/contact/mail-dark.png' alt='email' className={'w-20 h-auto'}/>
+              <img src='src/assets/images/contact/mail-light.png' alt='email' className={'w-20 h-auto xl:w-32'}/>
+              : <img src='src/assets/images/contact/mail-dark.png' alt='email' className={'w-20 h-auto xl:w-32'}/>
           }
           <h2 className={`text-4xl ${isCurrentLight ? 'text-purple-dark' : 'text-violet-light'}`}>Email</h2>
         </a>
