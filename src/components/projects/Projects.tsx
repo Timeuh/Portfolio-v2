380b29b3e8de5a7aa89af6e826ad33d884ee6c4b
Timeuh <timeuuh@gmail.com>
2023-07-04 11:05:24 +0200

    feat: adapt to desktop

--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -1,10 +1,11 @@
 import {projects} from '@/utils/Projects';
-import ProjectItem from '@/components/projects/ProjectItem';
+import ProjectItem from '@/components/projects/mobile/ProjectItem';
 import {useContext} from 'react';
 import {LangContext, ThemeContext} from '@/App';
 import {lang, theme} from '@/utils/AppTypes';
 import {isLight} from '@/utils/AppFuncs';
 import {projectsContent} from '@/utils/TextContent';
+import ProjectCarousel from '@/components/projects/desktop/ProjectCarousel';
 
 export default function Projects() {
   // get the theme from provider
@@ -18,20 +19,21 @@ export default function Projects() {
   const currentTrad = projectsContent[lang];
 
   return (
-    <div id={'projects'} className={`h-full w-full flex flex-col items-center justify-center space-y-12 py-12 
+    <div id={'projects'} className={`h-full w-full flex flex-col items-center justify-center space-y-12 py-12 xl:h-screen
       ${isCurrentLight ? 'bg-green-light' : 'bg-green-dark'}`}>
       {
         isCurrentLight ?
-          <img src='src/assets/images/projects/utils/projects-light.png' alt='project icon'/>
-          : <img src='src/assets/images/projects/utils/projects-dark.png' alt='project icon'/>
+          <img src='src/assets/images/projects/utils/projects-light.png' alt='project icon' className={'xl:w-32'}/>
+          : <img src='src/assets/images/projects/utils/projects-dark.png' alt='project icon' className={'xl:w-32'}/>
       }
-      <h1 className={`text-5xl font-bold ${isCurrentLight ? 'text-green-dark' : 'text-green-light'}`}>{currentTrad.title}</h1>
+      <h1 className={`text-5xl font-bold ${isCurrentLight ? 'text-green-dark' : 'text-green-light'} xl:text-6xl`}>{currentTrad.title}</h1>
       {
         projects.map((project, index)  => {
           return <ProjectItem key={index} name={project.name} gif={project.gif} description={project.description}
             techs={project.techs} views={project.views}/>;
         })
       }
+      <ProjectCarousel projects={projects}/>
     </div>
   );
 }
